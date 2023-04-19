fb344699a1f148faafbeb0d3aa86a52979a4b9aa
maks <maxim_86nva@mail.ru>
2023-04-19 17:33:26 +0300

    init project

---
+++ src/main/java/com/example/hwswaggerui/HwSwaggerUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwswaggerui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSwaggerUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwSwaggerUiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hwswaggerui/HwSwaggerUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwswaggerui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSwaggerUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
